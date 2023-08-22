454cfad567bb3b9801cfe45e7ab3fa36b75c2453
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-22 00:36:57 -0700

    Perms

--- nginx/set-perms.sh
+++ nginx/set-perms.sh
@@ -2,11 +2,13 @@
 set -e
 
 echo "Running: set-perms.sh"
-echo "SUID: ${SUID}"
 
-if [ -n "${SUID}" ];then
-    echo "Setting Permissions ${SUID}:${SUID} on /static"
-    chown -R "${SUID}:${SUID}" "/static"
-fi
+echo "STATIC: ${STATIC:-/static}"
+echo "SUID: ${SUID:-1000}"
+
+#if [ -n "${SUID}" ];then
+echo "Setting Permissions ${SUID:-1000}:${SUID:-1000} on ${STATIC:-/static}"
+chown -R "${SUID:-1000}:${SUID:-1000}" "${STATIC:-/static}"
+#fi
 
 echo "Done"
